c33b286488d5f9efa2b12d8041811c751c0554fb
Mostafa Kassem <moustafakassem12@gmail.com>
2021-03-07 19:05:30 +0200

    Completed Step_5 JS2

---
+++ JS-Basics-master/javascript-basics-02/step 5/Step_5.js
@@ -0,0 +1,18 @@
+function pic() {
+    const im1 = document.querySelector("#image1");
+    const im2 = document.querySelector("#image2");
+    const im3 = document.querySelector("#image3");
+    const im4 = document.querySelector("#image4");
+    const im5 = document.querySelector("#image5");
+
+    im1.src = "images/image1_2.jpg"
+
+    im2.src = "images/image2_2.jpg"
+
+    im3.src = "images/image3_2.jpg"
+
+    im4.src = "images/image4_2.jpg"
+
+    im5.src = "images/image5_2.jpg"
+
+}
